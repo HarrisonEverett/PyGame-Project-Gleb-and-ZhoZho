5b1926ee49c3a1611390253162f65d4058407d4c
HarrisonEverett <40396844+HarrisonEverett@users.noreply.github.com>
2020-02-09 15:36:27 +0600

    ABILITY TO SAVE WAS IMPLEMENTED

--- qq3.py
+++ qq3.py
@@ -1,48 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-This is sample of how you can implement a tile-based game, not unlike
-the RPG games known from consoles, in pygame. It's not a playable game,
-but it can be turned into one. Care has been taken to comment it clearly,
-so that you can use it easily as a starting point for your game.
-
-The program reads a level definition from a "level.map" file, and uses the
-graphics referenced for that file to display a tiled map on the screen and
-let you move an animated player character around it.
-
-Note that a lot of additional work is needed to turn it into an actual game.
-
-@copyright: 2008, 2009 Radomir Dopieralski <qq@sheep.art.pl>
-@license: BSD, see COPYING for details
-
-"""
 
 import configparser
+
+import pickle
 
 import pygame
 import pygame.locals as pg
 
-# Motion offsets for particular directions
-#     N  E  S   W
 DX = [0, 1, 0, -1]
 DY = [-1, 0, 1, 0]
 
-# Dimensions of the map tiles
 MAP_TILE_WIDTH, MAP_TILE_HEIGHT = 24, 16
 FPS = 15
 
-class TileCache(object):
-    """Load the tilesets lazily into global cache"""
 
+last_save = []
+
+
+class TileCache(object):
+    
+    
     def __init__(self,  width=32, height=None):
         self.width = width
         self.height = height or width
         self.cache = {}
 
     def __getitem__(self, filename):
-        """Return a table of tiles, load it from disk if needed."""
-
+    
         key = (filename, self.width, self.height)
         try:
             return self.cache[key]
@@ -53,8 +39,6 @@ class TileCache(object):
             return tile_table
 
     def _load_tile_table(self, filename, width, height):
-        """Load an image and split it into tiles."""
-
         image = pygame.image.load(filename).convert()
         image_width, image_height = image.get_size()
         tile_table = []
@@ -68,17 +52,11 @@ class TileCache(object):
 
 
 class SortedUpdates(pygame.sprite.RenderUpdates):
-    """A sprite group that sorts them by depth."""
-
     def sprites(self):
-        """The list of sprites in the group, sorted by depth."""
-
         return sorted(list(self.spritedict.keys()), key=lambda sprite: sprite.depth)
 
 
 class Shadow(pygame.sprite.Sprite):
-    """Sprite for shadows."""
-
     def __init__(self, owner):
         pygame.sprite.Sprite.__init__(self)
         self.image = SPRITE_CACHE["shadow.png"][0][0]
@@ -87,14 +65,10 @@ class Shadow(pygame.sprite.Sprite):
         self.owner = owner
 
     def update(self, *args):
-        """Make the shadow follow its owner."""
-
         self.rect.midbottom = self.owner.rect.midbottom
 
 
 class Sprite(pygame.sprite.Sprite):
-    """Sprite for animated items and base class for Player."""
-
     is_player = False
 
     def __init__(self, pos=(0, 0), frames=None):
@@ -107,44 +81,31 @@ class Sprite(pygame.sprite.Sprite):
         self.pos = pos
 
     def _get_pos(self):
-        """Check the current position of the sprite on the map."""
-
         return ((self.rect.midbottom[0] - 12) // 24,
                 (self.rect.midbottom[1] - 16) // 16)
 
     def _set_pos(self, pos):
-        """Set the position and depth of the sprite on the map."""
-
         self.rect.midbottom = pos[0]*24+12, pos[1]*16+16
         self.depth = self.rect.midbottom[1]
 
     pos = property(_get_pos, _set_pos)
 
     def move(self, dx, dy):
-        """Change the position of the sprite on screen."""
-
         self.rect.move_ip(dx, dy)
         self.depth = self.rect.midbottom[1]
 
     def stand_animation(self):
-        """The default animation."""
-
         while True:
-            # Change to next frame every two ticks
             for frame in self.frames[0]:
                 self.image = frame
                 yield None
                 yield None
 
     def update(self, *args):
-        """Run the current animation."""
-
         next(self.animation)
 
 
 class Player(Sprite):
-    """ Display and animate the player character."""
-
     is_player = True
 
     def __init__(self, pos=(1, 1)):
@@ -155,9 +116,6 @@ class Player(Sprite):
         self.image = self.frames[self.direction][0]
 
     def walk_animation(self, multiplier):
-        """Animation for the player walking."""
-
-        # This animation is hardcoded for 4 frames and 16x24 map tiles
         for frame in range(4):
             self.image = self.frames[self.direction][frame]
             yield None
@@ -166,8 +124,6 @@ class Player(Sprite):
             self.move(3*multiplier*DX[self.direction], 2*multiplier*DY[self.direction])
 
     def update(self, *args):
-        """Run the current animation or just stand there if no animation set."""
-
         if self.animation is None:
             self.image = self.frames[self.direction][0]
         else:
@@ -177,8 +133,8 @@ class Player(Sprite):
                 self.animation = None
 
 class Level(object):
-    """Load and store the map of the level, together with all the items."""
-
+    
+    
     def __init__(self, filename="level.map"):
         self.tileset = ''
         self.map = []
@@ -189,8 +145,6 @@ class Level(object):
         self.load_file(filename)
 
     def load_file(self, filename="level.map"):
-        """Load the level from specified file."""
-
         parser = configparser.ConfigParser()
         parser.read(filename)
         self.tileset = parser.get("level", "tileset")
@@ -207,8 +161,6 @@ class Level(object):
                     self.items[(x, y)] = self.key[c]
 
     def render(self):
-        """Draw the level on the surface."""
-
         wall = self.is_wall
         tiles = MAP_CACHE[self.tileset]
         image = pygame.Surface((self.width*MAP_TILE_WIDTH, self.height*MAP_TILE_HEIGHT))
@@ -216,7 +168,6 @@ class Level(object):
         for map_y, line in enumerate(self.map):
             for map_x, c in enumerate(line):
                 if wall(map_x, map_y):
-                    # Draw different tiles depending on neighbourhood
                     if not wall(map_x, map_y+1):
                         if wall(map_x+1, map_y) and wall(map_x-1, map_y):
                             tile = 1, 2
@@ -235,7 +186,6 @@ class Level(object):
                             tile = 2, 1
                         else:
                             tile = 3, 1
-                    # Add overlays if the wall may be obscuring something
                     if not wall(map_x, map_y-1):
                         if wall(map_x+1, map_y) and wall(map_x-1, map_y):
                             over = 1, 0
@@ -251,7 +201,6 @@ class Level(object):
                         tile = self.key[c]['tile'].split(',')
                         tile = int(tile[0]), int(tile[1])
                     except (ValueError, KeyError):
-                        # Default to ground tile
                         tile = 0, 3
                 tile_image = tiles[tile[0]][tile[1]]
                 image.blit(tile_image,
@@ -259,8 +208,7 @@ class Level(object):
         return image, overlays
 
     def get_tile(self, x, y):
-        """Tell what's at the specified position of the map."""
-
+        
         try:
             char = self.map[y][x]
         except IndexError:
@@ -271,27 +219,20 @@ class Level(object):
             return {}
 
     def get_bool(self, x, y, name):
-        """Tell if the specified flag is set for position on the map."""
-
         value = self.get_tile(x, y).get(name)
         return value in (True, 1, 'true', 'yes', 'True', 'Yes', '1', 'on', 'On')
 
     def is_wall(self, x, y):
-        """Is there a wall?"""
-
         return self.get_bool(x, y, 'wall')
 
     def is_blocking(self, x, y):
-        """Is this place blocking movement?"""
-
         if not 0 <= x < self.width or not 0 <= y < self.height:
             return True
         return self.get_bool(x, y, 'block')
 
 
 class Game(object):
-    """The main game object."""  
-
+    
     def __init__(self):
         self.screen = pygame.display.get_surface()
         self.pressed_key = None
@@ -301,27 +242,32 @@ class Game(object):
         self.overlays = pygame.sprite.RenderUpdates()
         
         self.features = ["bush", "forward", "backward", "stop", "crate"]
+        self.interact = []
+        
         
         self.bushstuff = []
-        self.interact = []
-        self.potato = 499
+        self.potato = 1000
         self.weight = False
         self.weightnum = 250
         self.maxcapacity = 500
         self.donation = 0
         self.needed = 1000
-        self.deadline = 10 * FPS
+        self.deadline = 99999 * FPS
         self.stoppedtime = 25 * FPS
+        
         self.is_time_stopped = False
         
         self.is_game_paused = False
         
-        self.save = []
+        self.good_ending = False
+        
+        self.last_save = []
         
         self.use_level(Level())        
 
     def use_level(self, level):
-        """Set the level as the current one."""
+        global last_save
+        
         def around(pos, name):
             x, y = pos
             place = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
@@ -336,11 +282,9 @@ class Game(object):
         self.sprites = SortedUpdates()
         self.overlays = pygame.sprite.RenderUpdates()
         self.level = level
-        # Populate the game with the level's objects
         for pos, tile in level.items.items():
             if tile['name'] in self.features:
                 x, y = pos
-                #self.interact.append([tile['name'], (x, y)])                
                 around(pos, tile['name'])
                 
             if tile.get("player") in ('true', '1', 'yes', 'on'):
@@ -350,27 +294,20 @@ class Game(object):
                 sprite = Sprite(pos, SPRITE_CACHE[tile["sprite"]])
             self.sprites.add(sprite)
             self.shadows.add(Shadow(sprite))
-        # Render the level map
         self.background, overlays = self.level.render()
-        # Add the overlays for the level map
         for (x, y), image in overlays.items():
             overlay = pygame.sprite.Sprite(self.overlays)
             overlay.image = image
             overlay.rect = image.get_rect().move(x*24, y*16-16)
 
     def control(self):
-        """Handle the controls of the game."""
-
         keys = pygame.key.get_pressed()
 
         def pressed(key):
-            """Check if the specified key is pressed."""
-
             return self.pressed_key == key or keys[key]
 
         def walk(d, is_run):
-            """Start walking in specified direction."""
-
+    
             x, y = self.player.pos
             self.player.direction = d
             multiplier = 1
@@ -454,7 +391,7 @@ class Game(object):
                         scoretext = myfont.render("TIME LEFT TO GROW: {0}".format(self.bushstuff[bush][2] // FPS), 1, (255,255,102))
                         self.screen.blit(scoretext, (5, 330))                        
                 
-                if pos in i[1] and self.potato > 0  and i[0] == 'house':
+                if pos in i[1] and self.potato > 0  and i[0] == 'crate':
                     coef = 0
                     maximum = 500
                     while maximum != 0:
@@ -496,6 +433,21 @@ class Game(object):
                 self.is_game_paused = True
             else:
                 self.is_game_paused = False
+        elif keys[pg.K_o]:
+            
+            f = open("save.txt", "w", encoding="utf-8")
+            f.write(str(self.potato))
+            f.write('\n')
+            f.write(str(self.donation))
+            f.write('\n')
+            f.write(str(self.deadline))
+            f.close()
+                                    
+        elif keys[pg.K_i]:
+            lineList = [line.rstrip('\n') for line in open('save.txt')]
+            self.potato = int(lineList[0])
+            self.donation = int(lineList[1])
+            self.deadline = int(lineList[2])
         self.pressed_key = None        
         
 
@@ -517,7 +469,11 @@ class Game(object):
                 
         def game_over():
             self.screen.fill((0, 0, 0))
-            end_img = pygame.image.load('end.png')
+            if self.good_ending is True:
+                state = "win.png"
+            else:
+                state = "end.png"
+            end_img = pygame.image.load(state)
             end_img_rect = end_img.get_rect()
             end_img_rect.center = (MAP_TILE_WIDTH * 35 / 2, MAP_TILE_HEIGHT * 23 / 2)
             self.screen.blit(end_img, end_img_rect)
@@ -567,50 +523,30 @@ class Game(object):
         start_game()
 
         clock = pygame.time.Clock()
-        # Draw the whole screen initially
         self.screen.blit(self.background, (0, 0))
         self.overlays.draw(self.screen)
         pygame.display.flip()
-        # The main game loop
-            
+           
         pygame.mixer.music.stop()
         music = pygame.mixer.Sound('music.wav')
         music.play()                           
         while not self.game_over:
-            
-            
-            
-            
             myfont = pygame.font.SysFont("monospace", 16)
             self.screen.fill((0,0,0))
-            
-            self.screen.blit(self.background, (0, 0))
+                     
             
             if self.is_game_paused is True:
-                scoretext = myfont.render("PAUSE", 1, (255,255,255))
-                self.screen.blit(scoretext, (5, 210))                
+                scoretext = myfont.render("It is okay, gardener, take your time.", 1, (255,255,255))
+                self.screen.blit(scoretext, (MAP_TILE_WIDTH * 35 / 2 - 200, MAP_TILE_HEIGHT * 23 / 2))                
                 for event in pygame.event.get():
                     if event.type == pg.KEYDOWN:    
                         self.is_game_paused = False  
                 pygame.display.flip()
                 continue            
             
+            display()   
             
-            if self.is_time_stopped is True:
-                timestop()
-                    
-            display()      
-            
-            if self.deadline != 0 and self.is_time_stopped is False:
-                self.deadline -= 1
-            if self.potato >= self.weightnum:
-                self.weight = True
-            else:
-                self.weight = False
-            
-            if self.is_time_stopped is False:
-                potatogrow()
-            
+            self.screen.blit(self.background, (0, 0))
             # Don't clear shadows and overlays, only sprites.
             self.sprites.clear(self.screen, self.background)
             self.sprites.update()
@@ -630,9 +566,28 @@ class Game(object):
             pygame.display.update(dirty)
             # Wait for one tick of the game clock
             clock.tick(15)
-            # Process pygame events
+            # Process pygame events               
+            
+            if self.is_time_stopped is True:
+                timestop()
+                    
+            if self.deadline != 0 and self.is_time_stopped is False:
+                self.deadline -= 1
+                
+            if self.potato >= self.weightnum:
+                self.weight = True
+            else:
+                self.weight = False
+            
+            if self.is_time_stopped is False:
+                potatogrow()
+            
+           
             for event in pygame.event.get():
                 if self.deadline == 0 and self.needed != 0:
+                    self.game_over = True
+                if self.donation == self.needed:
+                    self.good_ending = True
                     self.game_over = True
                 if event.type == pg.QUIT:
                     self.game_over = True
@@ -640,17 +595,10 @@ class Game(object):
                     self.pressed_key = event.key
                     
             pygame.display.flip()
-        game_over()               
-        
-                    
+        game_over()
             
-#I didnt enclude a start or end screen picure
-#Just rename the picture start.png and gameover.png
-#I encluded some music in too when u die
-
-
-
-           
+        
+                          
 if __name__ == "__main__":
     SPRITE_CACHE = TileCache()
     MAP_CACHE = TileCache(MAP_TILE_WIDTH, MAP_TILE_HEIGHT)
